c3aad00d62980e886776357d48b10e1223fa0cd4
manukhatri <khatrimanu604@gmail.com>
2026-02-24 07:36:11 +0000

    day1

---
+++ myfirstproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1>This is inside app component</h1>
+
+    <h1>Manu khatri</h1>
+    </div>
+  )
+}
+
+export default App
