2f1d0a3edad329990d9040442bc6e720c410b87a
manukhatri <khatrimanu604@gmail.com>
2026-04-21 05:28:36 +0000

    Day 9

--- myfirstproject/src/App.jsx
+++ myfirstproject/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Demo from './Component/Demo';
-import Assignment1 from './Component/Assignment1'
+import Demo from './Assignment/Demo';
+import Assignment1 from './Assignment/Assignment1'
 function App() {
  
   return (
